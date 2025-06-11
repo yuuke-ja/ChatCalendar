714a06a3b5d071f109856b45037bdb68df776af4
yuuke-ja <youjiezhongwei2@gmail.com>
2025-06-11 11:18:11 +0900

    スマホ画面に合わせる

--- prisma/migrations/20250516061837_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "content" TEXT NOT NULL,
-    "postedBy" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("createdAt")
-);
